b4a5bfc8957d73542a271a814ecc9c9e272989bd
bbulliard <bbulliard@gmail.com>
2022-02-02 17:07:50 -0600

    20220201_1705

--- oil_gas_calculations/ContentView.swift
+++ oil_gas_calculations/ContentView.swift
@@ -36,7 +36,8 @@ struct ContentView: View {
                     NavigationLink("Pressure Gradient bar/m - Using drill fluid density kg/l", destination: nextview4())
                     NavigationLink("Pressure Gradient bar/10m - Using drill fluid density kg/l", destination: nextview5())
                     NavigationLink("S.i units calc - pressure gradient using drilling fluid density kg/m**³", destination: nextview6())
-                   NavigationLink("Convert pressure gradient, psi/ft to mud weight, ppg using pressure gradient, psi/ft", destination: nextview7())
+            NavigationLink("Convert pressure gradient to mud weight(ppg, lb/ft,sg) using psi/ft", destination: nextview7())
+        NavigationLink("Convert Drilling fluid density, kg/l using pressure gradient bar/m and bar/10m", destination: nextview8())
                      }
                 }
             }
@@ -151,18 +152,52 @@ struct nextview7: View {
                         .keyboardType(.decimalPad)
             let amtx = (amt as NSString).doubleValue
         Text("ppg/ mud weight: \(convtomud(parm1: amtx, parm2: 2))")
+                .background(.green)
+        Text("lb/ft³/ mud weight: \(convtomud2(parm1: amtx, parm2: 2))")
+                .background(.green)
+        Text("sg/ mud weight: \(convtomud3(parm1: amtx, parm2: 2))")
         .background(.green)}}
 }
 func convtomud(parm1: Double, parm2: Double) -> Double {
         var total: Double = 0
     total = parm1 / 0.052
         return total}
+func convtomud2(parm1: Double, parm2: Double) -> Double {
+        var total: Double = 0
+    total = parm1 / 0.006944
+        return total}
+func convtomud3(parm1: Double, parm2: Double) -> Double {
+        var total: Double = 0
+    total = parm1 / 0.433
+        return total}
 
+struct nextview8: View {
+    @State private var amt = ""
+    @State private var amt2 = ""
+    //var amt = ""
+    var body: some View {
+        VStack{
+            TextField("Enter pressure gradient, bar/m", text: $amt)
+                .background(.green)
+            TextField("Enter pressure gradient, bar/10m", text: $amt2)
+           .background(.green)
+                        .keyboardType(.decimalPad)
+            let amtx = (amt as NSString).doubleValue
+            let amtx2 = (amt2 as NSString).doubleValue
+        Text("Drilling fluid Density, kg/l using bar/m:  \(drill_fluid_density(parm1: amtx, parm2: 2))")
+            .background(.green)
+    Text("Drilling fluid Density, kg/l using bar/10m:   \(drill_fluid_density2(parm1: amtx2, parm2: 2))")
+        .background(.green)}}
+}
 
-
-
-
-
+func drill_fluid_density(parm1: Double, parm2: Double) -> Double {
+        var total: Double = 0
+    total = parm1 / 0.0981
+        return total}
+func drill_fluid_density2(parm1: Double, parm2: Double) -> Double {
+        var total: Double = 0
+    total = parm1 / 0.981
+        return total}
 
 
 
